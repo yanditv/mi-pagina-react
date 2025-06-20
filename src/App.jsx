6797ee4513dfbbbb2256baeb4c0467b0a20a87ad
yanditv <yankur44@gmail.com>
2025-06-20 14:59:32 -0500

    mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1>Hola mundo cruel</h1>
+        <p>mi vida es un pesadilla con los estudiantes de la unidad acemica</p>
+      </div>
+    </>
+  );
+}
